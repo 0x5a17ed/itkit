669502250c086bd7c135b3d67d211da18346ab7c
x5a17ed <0x5a17ed@tuta.io>
2022-08-17 07:40:10 +0200

    move string tests to itkit_test package to enforce behavioral testing

--- string_test.go
+++ string_test.go
@@ -12,27 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package itkit
+package itkit_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/0x5a17ed/itkit"
 )
 
 func TestString(t *testing.T) {
 	t.Run("unicode", func(t *testing.T) {
-		s := ToSlice(Runes("日本\x80語"))
+		s := itkit.ToSlice(itkit.Runes("日本\x80語"))
 		assert.Equal(t, []rune{0x65E5, 0x672C, 0xFFFD, 0x8A9E}, s)
 	})
 
 	t.Run("ascii", func(t *testing.T) {
-		s := ToSlice(Runes("Hello World"))
+		s := itkit.ToSlice(itkit.Runes("Hello World"))
 		assert.Equal(t, []rune{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64}, s)
 	})
 
 	t.Run("late unicode", func(t *testing.T) {
-		s := ToSlice(Runes("Hello Wörld"))
+		s := itkit.ToSlice(itkit.Runes("Hello Wörld"))
 		assert.Equal(t, []rune{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0xf6, 0x72, 0x6c, 0x64}, s)
 	})
 }
