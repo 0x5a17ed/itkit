46ccaf3ed19a87f3a983810a4d1a41bf4e4a0121
x5a17ed <0x5a17ed@tuta.io>
2024-05-18 14:21:40 +0200

    ioit: fix an edge case in generator close down and cleanup

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/0x5a17ed/itkit
 go 1.20
 
 require (
-	github.com/0x5a17ed/coro v1.0.0
+	github.com/0x5a17ed/coro v1.1.0
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/goleak v1.3.0
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
--- iters/ioit/ioit_test.go
+++ iters/ioit/ioit_test.go
@@ -16,19 +16,99 @@ package ioit_test
 
 import (
 	"io/fs"
+	"sync"
 	"testing"
 
 	"github.com/0x5a17ed/itkit/iters/ioit"
+	"github.com/0x5a17ed/itkit/iters/sliceit"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/goleak"
 )
+
+type Log struct {
+	mx      sync.Mutex
+	Entries []string
+}
+
+func (l *Log) Add(s string) {
+	l.mx.Lock()
+	defer l.mx.Unlock()
+
+	l.Entries = append(l.Entries, s)
+}
+
+func (l *Log) Assert(t *testing.T, s []string) {
+	assert.Equal(t, s, l.Entries)
+}
+
+func singleGeneratorFactory(l *Log) *ioit.Generator[int] {
+	l.Add("factory creating generator")
+	gen := ioit.Run(func(cont bool, yield func(int) bool) error {
+		l.Add("generator enter")
+		defer l.Add("generator leave")
+
+		if !cont {
+			l.Add("generator closing down")
+			return nil
+		}
+
+		l.Add("generator yielding")
+		cont = yield(1)
+		l.Add("generator yielded")
+
+		if !cont {
+			l.Add("generator closing down")
+			return fs.ErrClosed
+		}
+
+		l.Add("generator returning")
+		return fs.ErrNotExist
+	})
+	l.Add("factory returning generator")
+
+	return gen
+}
+
+func badGeneratorFactory(l *Log) *ioit.Generator[int] {
+	l.Add("factory creating generator")
+	gen := ioit.Run(func(cont bool, yield func(int) bool) error {
+		l.Add("generator enter")
+		defer l.Add("generator leave")
+
+		for i := 1; ; i++ {
+			l.Add("generator yielding")
+			yield(i)
+			l.Add("generator yielded")
+		}
+	})
+	l.Add("factory returning generator")
+
+	return gen
+}
+
+func panicGeneratorFactory(l *Log) *ioit.Generator[int] {
+	l.Add("factory creating generator")
+	gen := ioit.Run(func(cont bool, yield func(int) bool) error {
+		l.Add("generator enter")
+		defer l.Add("generator leave")
+
+		l.Add("generator panic")
+		panic(nil)
+	})
+	l.Add("factory returning generator")
+
+	return gen
+}
 
 func TestGenerator_Err(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
 	asserter := assert.New(t)
 
-	g := ioit.Run(func(func(any)) error {
+	g := ioit.Run(func(cont bool, _ func(any) bool) error {
+		if !cont {
+			return nil
+		}
 		return fs.ErrNotExist
 	})
 
@@ -37,4 +117,136 @@ func TestGenerator_Err(t *testing.T) {
 	asserter.False(g.Next())
 
 	asserter.ErrorIs(g.Err(), fs.ErrNotExist)
+}
+
+func TestGenerator_Close(t *testing.T) {
+	t.Run("after completion", func(t *testing.T) {
+		asserter := assert.New(t)
+
+		var l Log
+		g := singleGeneratorFactory(&l)
+
+		// Exhaust the iterator.
+		asserter.Equal([]int{1}, sliceit.To(g.Iter()))
+
+		// Signal the generator to close down.
+		l.Add("caller closing")
+		asserter.ErrorIs(g.Close(), fs.ErrNotExist)
+
+		l.Assert(t, []string{
+			"factory creating generator",
+			"factory returning generator",
+			"generator enter",
+			"generator yielding",
+			"generator yielded",
+			"generator returning",
+			"generator leave",
+			"caller closing",
+		})
+	})
+
+	t.Run("before start", func(t *testing.T) {
+		asserter := assert.New(t)
+
+		var l Log
+		g := singleGeneratorFactory(&l)
+
+		// Signal the generator to close down.
+		l.Add("caller closing")
+		asserter.ErrorIs(g.Close(), nil)
+		l.Add("caller closed")
+
+		// Assert the generator has reported no error.
+		asserter.NoError(g.Err())
+
+		// assert the state at the end.
+		l.Assert(t, []string{
+			"factory creating generator",
+			"factory returning generator",
+			"caller closing",
+			"generator enter",
+			"generator closing down",
+			"generator leave",
+			"caller closed",
+		})
+	})
+
+	t.Run("running", func(t *testing.T) {
+		asserter := assert.New(t)
+
+		var l Log
+		g := singleGeneratorFactory(&l)
+
+		// Retrieve first value from the generator.
+		l.Add("caller resuming")
+		ok := g.Next()
+		l.Add("caller resumed")
+		asserter.True(ok)
+
+		// Signal the generator to close down.
+		l.Add("caller closing")
+		asserter.ErrorIs(g.Close(), fs.ErrClosed)
+		l.Add("caller closed")
+
+		// assert the state at the end.
+		l.Assert(t, []string{
+			"factory creating generator",
+			"factory returning generator",
+			"caller resuming",
+			"generator enter",
+			"generator yielding",
+			"caller resumed",
+			"caller closing",
+			"generator yielded",
+			"generator closing down",
+			"generator leave",
+			"caller closed",
+		})
+	})
+
+	t.Run("bogus", func(t *testing.T) {
+		asserter := assert.New(t)
+
+		var l Log
+		g := badGeneratorFactory(&l)
+
+		// Signal the generator to close down.
+		l.Add("caller closing")
+		asserter.ErrorIs(g.Close(), ioit.ErrStopped)
+		l.Add("caller closed")
+
+		l.Assert(t, []string{
+			"factory creating generator",
+			"factory returning generator",
+			"caller closing",
+			"generator enter",
+			"generator yielding",
+			"generator leave",
+			"caller closed",
+		})
+	})
+
+	t.Run("panic", func(t *testing.T) {
+		asserter := assert.New(t)
+
+		var l Log
+		g := panicGeneratorFactory(&l)
+
+		// Signal the generator to close down.
+		l.Add("caller closing")
+		asserter.Panics(func() {
+			_ = g.Close()
+		})
+		l.Add("caller closed")
+
+		l.Assert(t, []string{
+			"factory creating generator",
+			"factory returning generator",
+			"caller closing",
+			"generator enter",
+			"generator panic",
+			"generator leave",
+			"caller closed",
+		})
+	})
 }
