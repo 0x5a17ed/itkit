812f4e7b4a4f58231db7881fbfd7510b9bb66579
x5a17ed <0x5a17ed@tuta.io>
2024-05-21 17:31:05 +0200

    limit: add iterator limiting other iterators

---
+++ itlib/limit.go
@@ -0,0 +1,48 @@
+// Copyright (c) 2024 individual contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// <https://www.apache.org/licenses/LICENSE-2.0>
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package itlib
+
+import (
+	"github.com/0x5a17ed/itkit"
+)
+
+// LimitIterator is an iterator yielding up to n items from the
+// provided source iterator.
+type LimitIterator[T any] struct {
+	src itkit.Iterator[T]
+	n   uint
+}
+
+// Ensure LimitIterator implements the iterator interface.
+var _ itkit.Iterator[struct{}] = &LimitIterator[struct{}]{}
+
+// Next implements the [itkit.Iterator.Next] interface.
+func (it *LimitIterator[T]) Next() bool {
+	if it.n == 0 || !it.src.Next() {
+		return false
+	}
+	it.n -= 1
+	return true
+}
+
+// Value implements the [itkit.Iterator.Value] interface.
+func (it *LimitIterator[T]) Value() (v T) {
+	return it.src.Value()
+}
+
+// Limit returns a new [LimitIterator] instance.
+func Limit[T any](src itkit.Iterator[T], n uint) itkit.Iterator[T] {
+	return &LimitIterator[T]{src: src, n: n}
+}
---
+++ itlib/limit_test.go
@@ -0,0 +1,60 @@
+// Copyright (c) 2024 individual contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// <https://www.apache.org/licenses/LICENSE-2.0>
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package itlib_test
+
+import (
+	"testing"
+
+	"github.com/0x5a17ed/itkit/iters/rangeit"
+	"github.com/0x5a17ed/itkit/iters/sliceit"
+	"github.com/0x5a17ed/itkit/itlib"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLimit(t *testing.T) {
+	t.Run("empty", func(t *testing.T) {
+		it := itlib.Limit(itlib.Empty[any](), 10)
+		assert.False(t, it.Next())
+	})
+
+	t.Run("less", func(t *testing.T) {
+		asserter := assert.New(t)
+
+		src := rangeit.Range(10)
+
+		it := itlib.Limit(src, 4)
+
+		asserter.Equal([]int{0, 1, 2, 3}, sliceit.To(it))
+
+		asserter.False(it.Next())
+
+		asserter.True(src.Next())
+		asserter.Equal(4, src.Value())
+	})
+
+	t.Run("more", func(t *testing.T) {
+		asserter := assert.New(t)
+
+		src := rangeit.Range(4)
+
+		it := itlib.Limit(src, 10)
+
+		asserter.Equal([]int{0, 1, 2, 3}, sliceit.To(it))
+
+		asserter.False(it.Next())
+
+		asserter.False(src.Next())
+	})
+}
